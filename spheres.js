07a0a4ced53a415ab8e6435cc8d8122a0db02072
Bazhenov <garakin2@proton.me>
2025-10-04 12:22:17 +0300

    star_control_2_extended
    + star_control_2_extended
    + Рассказ "B.S.S."

---
+++ spheres.js
@@ -0,0 +1,219 @@
+// starmap-spheres.js
+window.sphereDataFromFile = {
+    none: [],
+    newSpheres:  [
+    {
+        name: "Ilwrath",
+        color: "#bc00dc",
+        x: 4.8,
+        y: 170.0,
+        size: 141.0
+    },
+    {
+        name: "Arilou",
+        color: "#0000ff",
+        x: 43.8,
+        y: 637.2,
+        size: 25.0
+    },
+    {
+        name: "Kohr-Ah",
+        color: "#666666",
+        x: 600.0,
+        y: 625.0,
+        size: 266.6
+    },
+    {
+        name: "Druuge",
+        color: "#bc0000",
+        x: 950.0,
+        y: 279.2,
+        size: 140.0
+    },
+    {
+        name: "Mycon",
+        color: "#b74dac",
+        x: 639.2,
+        y: 220.0,
+        size: 107.0
+    },
+    {
+        name: "Orz",
+        color: "#ac00c7",
+        x: 360.8,
+        y: 263.7,
+        size: 33.3
+    },
+    {
+        name: "Pkunk",
+        color: "#1f8594",
+        x: 50.2,
+        y: 40.1,
+        size: 66.6
+    },
+    {
+        name: "Spathi",
+        color: "#b75600",
+        x: 254.9,
+        y: 360.0,
+        size: 100.0
+    },
+    {
+        name: "Supox",
+        color: "#b75600",
+        x: 746.8,
+        y: 924.6,
+        size: 33.3
+    },
+    {
+        name: "Thraddash",
+        color: "#1f8594",
+        x: 253.5,
+        y: 835.8,
+        size: 83.3
+    },
+    {
+        name: "Umgah",
+        color: "#7365bc",
+        x: 179.8,
+        y: 600.0,
+        size: 83.3
+    },
+    {
+        name: "Ur-Quan",
+        color: "#00AA22",
+        x: 575.0,
+        y: 600.0,
+        size: 266.6
+    },
+    {
+        name: "Utwig",
+        color: "#4383a8",
+        x: 853.4,
+        y: 879.7,
+        size: 66.6
+    },
+    {
+        name: "Vux",
+        color: "#0000ff",
+        x: 441.2,
+        y: 155.8,
+        size: 90.0
+    },
+    {
+        name: "Yehat",
+        color: "#8246db",
+        x: 497.0,
+        y: 4.0,
+        size: 75.0
+    },
+    {
+        name: "Zoq-Fot-Pik",
+        color: "#bc0000",
+        x: 376.1,
+        y: 533.3,
+        size: 32.0
+    }
+    ],
+
+    oldSpheres: [
+    {
+        name: "Earthling",
+        color: "#f05015",
+        x: 177.0,
+        y: 144.5,
+        size: 44.0
+    },
+    {
+        name: "Chenjesu",
+        color: "#6090e0",
+        x: 70.0,
+        y: 218.0,
+        size: 64.0
+    },
+    {
+        name: "Mmrnmhrm",
+        color: "#808080",
+        x: 68.0,
+        y: 294.0,
+        size: 48.0
+    },
+    {
+        name: "Ilwrath",
+        color: "#E00040",
+        x: -10.0,
+        y: 355.0,
+        size: 53.0
+    },
+    {
+        name: "Spathi",
+        color: "#f0f000",
+        x: 245.0,
+        y: 365.0,
+        size: 100.0
+    },
+    {
+        name: "Androsynth",
+        color: "#e000f0",
+        x: 370.0,
+        y: 261.0,
+        size: 49.0
+    },
+    {
+        name: "Shofixti",
+        color: "#e07038",
+        x: 288.0,
+        y: 20.0,
+        size: 37.0
+    },
+    {
+        name: "Vux",
+        color: "#00f060",
+        x: 431.0,
+        y: 153.0,
+        size: 58.0
+    },
+    {
+        name: "Yehat",
+        color: "#F01BF0",
+        x: 495.0,
+        y: 5.0,
+        size: 105.0
+    },
+    {
+        name: "Mycon",
+        color: "#c0a810",
+        x: 624.0,
+        y: 235.0,
+        size: 56.0
+    },
+    {
+        name: "Umgah",
+        color: "#30d0a0",
+        x: 185.0,
+        y: 608.0,
+        size: 49.0
+    },
+    {
+        name: "Unknown",
+        color: "#6f6f6f",
+        x: 58.0,
+        y: 46.0,
+        size: 35.0
+    },
+    {
+        name: "Unknown",
+        color: "#6f6f6f",
+        x: 945.0,
+        y: 275.0,
+        size: 74.0
+    },
+    {
+        name: "Unknown",
+        color: "#6f6f6f",
+        x: 282.0,
+        y: 850.0,
+        size: 73.0
+    }
+    ]
+};
